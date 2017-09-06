8e02d03d2bb313e807400d0c21ac9274fbd070d3
dhoko <dhoko@cyaneus.org>
2017-09-06 17:01:12 +0200

    Remove warning from asmcrypto

--- build/pmcrypto.js
+++ build/pmcrypto.js
@@ -1,6 +1,10 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 'use strict';
 
+/**
+ * @link https://github.com/vibornoff/asmcrypto.js/issues/121
+ */
+asmCrypto.random.skipSystemRNGWarning = true;
 openpgp.config.integrity_protect = true;
 openpgp.config.use_native = true;
 openpgp.initWorker({ path: 'openpgp.worker.min.js' });
