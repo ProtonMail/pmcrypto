f9f179cce904c2de1a806d29ae77f168bc591285
KAYLukas <kay.lukas@gmail.com>
2018-02-23 15:27:59 +0100

    Add verifyPrimaryUser call to keyinfo

--- build/pmcrypto.js
+++ build/pmcrypto.js
@@ -1,4 +1,4 @@
-(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+(function(){function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s}return e})()({1:[function(require,module,exports){
 'use strict';
 
 /**
@@ -271,6 +271,11 @@ function keyInfo(rawKey, email) {
 
 
     return Promise.resolve().then(function () {
+        var keys = getKeys(rawKey);
+        return keys[0].verifyPrimaryUser().then(function () {
+            return keys;
+        });
+    }).then(function (keys) {
 
         var packetInfo = function packetInfo(packet, key) {
             if (!packet) {
@@ -320,7 +325,6 @@ function keyInfo(rawKey, email) {
             };
         };
 
-        var keys = getKeys(rawKey);
         var algoInfo = keys[0].primaryKey.getAlgorithmInfo();
 
         var obj = {
