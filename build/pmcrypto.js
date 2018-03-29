f66bcfeb0bda11a5d63cd35b6de88ab89ed8b136
KAYLukas <kay.lukas@gmail.com>
2018-03-29 15:18:51 +0200

    Fix

--- build/pmcrypto.js
+++ build/pmcrypto.js
@@ -1,4 +1,4 @@
-(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+(function(){function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s}return e})()({1:[function(require,module,exports){
 'use strict';
 
 /**
@@ -996,13 +996,16 @@ module.exports = {
 'use strict';
 
 // Load window.performance in the browser, perf_hooks in node, and fall back on Date
-var _ref = function () {
+var requirePerfHooks = function requirePerfHooks() {
     try {
-        return require('perf_hooks');
-    } catch (e) {
-        return { performance: window.performance };
-    }
-}(),
+        var result = require('perf_hooks');
+        if (result && result.performance) {
+            return result;
+        }
+    } catch (e) {}
+};
+
+var _ref = requirePerfHooks() || window || {},
     _ref$performance = _ref.performance,
     performance = _ref$performance === undefined ? Date : _ref$performance;
 
