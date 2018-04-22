51c0bf7ffe27b2dfe4728a6e2532578c3ba62515
KAYLukas <kay.lukas@gmail.com>
2018-04-22 15:23:07 +0200

    Fix pgp / inline signed only bugs

--- build/pmcrypto.js
+++ build/pmcrypto.js
@@ -765,6 +765,9 @@ var _require = require('../utils'),
     serverTime = _require.serverTime;
 
 function encryptMessage(options) {
+    if (typeof options.data === 'string') {
+        options.data = options.data.replace(/[ \t]*$/mg, '');
+    }
     options.date = typeof options.date === 'undefined' ? serverTime() : options.date;
     options.compression = options.compression ? openpgp.enums.compression.zlib : undefined;
     return openpgp.encrypt(options);
