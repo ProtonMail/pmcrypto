777c9bf8ed3b1101db28c1c6768abc16e253946c
mmso <mmso@protonmail.com>
2019-03-04 11:43:59 +0100

    Fix #53 - Signature mime parsing (#54)
    * Fix #53 - Signature mime parsing
    
    * More lenient parts parsing
    
    * Add test for extra part

---
+++ test/key/processMIME.spec.js
@@ -0,0 +1,46 @@
+import test from 'ava';
+import '../helper';
+
+import processMIME from '../../lib/message/processMIME';
+import { getKeys } from '../../lib';
+import { VERIFICATION_STATUS } from '../../lib/constants';
+import {
+    invalidMultipartSignedMessage,
+    multipartSignedMessage,
+    multipartSignedMessageBody,
+    extraMultipartSignedMessage,
+    key
+} from './processMIME.data';
+
+test('it can process multipart/signed mime messages and verify the signature', async (t) => {
+    const { body, verified } = await processMIME(
+        {
+            publicKeys: await getKeys(key)
+        },
+        multipartSignedMessage
+    );
+    t.is(verified, VERIFICATION_STATUS.SIGNED_AND_VALID);
+    t.is(body, multipartSignedMessageBody);
+});
+
+test('it can process multipart/signed mime messages and verify the signature with extra parts at the end', async (t) => {
+    const { body, verified } = await processMIME(
+        {
+            publicKeys: await getKeys(key)
+        },
+        extraMultipartSignedMessage
+    );
+    t.is(verified, VERIFICATION_STATUS.SIGNED_AND_VALID);
+    t.is(body, 'hello');
+});
+
+test('it does not verify invalid messages', async (t) => {
+    const { verified, body } = await processMIME(
+        {
+            publicKeys: await getKeys(key)
+        },
+        invalidMultipartSignedMessage
+    );
+    t.is(verified, VERIFICATION_STATUS.NOT_SIGNED);
+    t.is(body, 'message with missing signature');
+});
