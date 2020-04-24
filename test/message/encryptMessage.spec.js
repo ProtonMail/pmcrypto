0eb0c2f23817fcff3ee3eefe27b0f4a49b468e99
Daniel Huigens <d.huigens@protonmail.com>
2020-04-24 14:23:11 +0200

    Add tests for encrypt + decrypt roundtrip

---
+++ test/message/encryptMessage.spec.js
@@ -0,0 +1,49 @@
+import test from 'ava';
+import '../helper';
+
+import { createMessage, getMessage, getSignature, verifyMessage } from '../../lib/message/utils';
+import encryptMessage from '../../lib/message/encrypt';
+import { decryptMessage } from '../../lib/message/decrypt';
+import { decryptPrivateKey } from '../../lib';
+import { testPrivateKeyLegacy } from './decryptMessageLegacy.data';
+import { VERIFICATION_STATUS } from '../../lib/constants';
+
+test('it can encrypt and decrypt a message', async (t) => {
+    const decryptedPrivateKey = await decryptPrivateKey(testPrivateKeyLegacy, '123');
+    const { data: encrypted } = await encryptMessage({
+        message: createMessage('Hello world!'),
+        publicKeys: [decryptedPrivateKey.toPublic()],
+        privateKeys: [decryptedPrivateKey]
+    });
+    const { data: decrypted, verified } = await decryptMessage({
+        message: await getMessage(encrypted),
+        publicKeys: [decryptedPrivateKey.toPublic()],
+        privateKeys: [decryptedPrivateKey]
+    });
+    t.is(decrypted, 'Hello world!');
+    t.is(verified, VERIFICATION_STATUS.SIGNED_AND_VALID);
+});
+
+test('it can encrypt and decrypt a message with a detached signature', async (t) => {
+    const decryptedPrivateKey = await decryptPrivateKey(testPrivateKeyLegacy, '123');
+    const { data: encrypted, signature } = await encryptMessage({
+        message: createMessage('Hello world!'),
+        publicKeys: [decryptedPrivateKey.toPublic()],
+        privateKeys: [decryptedPrivateKey],
+        detached: true
+    });
+    const { data: decrypted, verified } = await decryptMessage({
+        message: await getMessage(encrypted),
+        signature: await getSignature(signature),
+        publicKeys: [decryptedPrivateKey.toPublic()],
+        privateKeys: [decryptedPrivateKey]
+    });
+    t.is(decrypted, 'Hello world!');
+    t.is(verified, VERIFICATION_STATUS.SIGNED_AND_VALID);
+    const { verified: verifiedAgain } = await verifyMessage({
+        message: createMessage('Hello world!'),
+        signature: await getSignature(signature),
+        publicKeys: [decryptedPrivateKey.toPublic()]
+    });
+    t.is(verifiedAgain, VERIFICATION_STATUS.SIGNED_AND_VALID);
+});
