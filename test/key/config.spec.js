ba03f463e28bffd35f3b211ce227440d605bd532
larabr <7375870+larabr@users.noreply.github.com>
2020-08-28 17:52:31 +0200

    Allow decryption with RSA signing keys to work around old bug (#95)

--- test/key/config.spec.js
+++ test/key/config.spec.js
@@ -3,6 +3,7 @@ import '../helper';
 import { openpgp } from '../../lib/openpgp';
 
 test('it sets the correct configuration on openpgp', async (t) => {
+    t.is(openpgp.config.allow_insecure_decryption_with_signing_keys, true);
     t.is(openpgp.config.s2k_iteration_count_byte, 96);
     t.is(openpgp.config.integrity_protect, true);
     t.is(openpgp.config.use_native, true);
