23da3049793cc90d5bfeebbbf002a66c0e9f6ee1
Marco <marco.martinoli@protonmail.com>
2020-09-24 11:27:10 +0000

    Add support for encrypted detached signatures (#97)

--- test/message/encryptMessage.spec.js
+++ test/message/encryptMessage.spec.js
@@ -24,7 +24,7 @@ test('it can encrypt and decrypt a message', async (t) => {
     t.is(verified, VERIFICATION_STATUS.SIGNED_AND_VALID);
 });
 
-test('it can encrypt and decrypt a message with a detached signature', async (t) => {
+test('it can encrypt and decrypt a message with an unencrypted detached signature', async (t) => {
     const decryptedPrivateKey = await decryptPrivateKey(testPrivateKeyLegacy, '123');
     const { data: encrypted, signature } = await encryptMessage({
         message: createMessage('Hello world!'),
@@ -46,4 +46,22 @@ test('it can encrypt and decrypt a message with a detached signature', async (t)
         publicKeys: [decryptedPrivateKey.toPublic()]
     });
     t.is(verifiedAgain, VERIFICATION_STATUS.SIGNED_AND_VALID);
+});
+
+test('it can encrypt and decrypt a message with an encrypted detached signature', async (t) => {
+    const decryptedPrivateKey = await decryptPrivateKey(testPrivateKeyLegacy, '123');
+    const { data: encrypted, encryptedSignature } = await encryptMessage({
+        message: createMessage('Hello world!'),
+        publicKeys: [decryptedPrivateKey.toPublic()],
+        privateKeys: [decryptedPrivateKey],
+        detached: true
+    });
+    const { data: decrypted, verified } = await decryptMessage({
+        message: await getMessage(encrypted),
+        encryptedSignature: await getMessage(encryptedSignature),
+        publicKeys: [decryptedPrivateKey.toPublic()],
+        privateKeys: [decryptedPrivateKey]
+    });
+    t.is(decrypted, 'Hello world!');
+    t.is(verified, VERIFICATION_STATUS.SIGNED_AND_VALID);
 });
