c9c3779b6eb769cac322fc44ca9b0115904a6f09
Marco <marco.martinoli@protonmail.com>
2020-09-29 17:10:33 +0200

    Fixed usage of session keys when returnSessionKey is not set

--- test/message/encryptMessage.spec.js
+++ test/message/encryptMessage.spec.js
@@ -4,7 +4,7 @@ import '../helper';
 import { createMessage, getMessage, getSignature, verifyMessage } from '../../lib/message/utils';
 import encryptMessage from '../../lib/message/encrypt';
 import { decryptMessage } from '../../lib/message/decrypt';
-import { decryptPrivateKey } from '../../lib';
+import { decryptPrivateKey, generateSessionKey } from '../../lib';
 import { testPrivateKeyLegacy } from './decryptMessageLegacy.data';
 import { VERIFICATION_STATUS } from '../../lib/constants';
 
@@ -92,6 +92,116 @@ test('it can encrypt a message and decrypt it unarmored using session keys along
         returnSessionKey: true,
         detached: true,
         armor: false
+    });
+    const { data: decrypted, verified } = await decryptMessage({
+        message: await getMessage(encrypted),
+        publicKeys: [decryptedPrivateKey.toPublic()],
+        encryptedSignature: await getMessage(encryptedSignature),
+        sessionKeys
+    });
+    t.is(decrypted, 'Hello world!');
+    t.is(verified, VERIFICATION_STATUS.SIGNED_AND_VALID);
+});
+
+test('it can encrypt and decrypt a message with session key without setting returnSessionKey', async (t) => {
+    const decryptedPrivateKey = await decryptPrivateKey(testPrivateKeyLegacy, '123');
+    const sessionKey = {
+        data: new Uint8Array([
+            197,
+            98,
+            157,
+            132,
+            15,
+            214,
+            78,
+            245,
+            90,
+            234,
+            71,
+            79,
+            135,
+            220,
+            222,
+            239,
+            118,
+            187,
+            199,
+            152,
+            163,
+            64,
+            239,
+            103,
+            4,
+            83,
+            21,
+            235,
+            121,
+            36,
+            163,
+            111
+        ]),
+        algorithm: 'aes256'
+    };
+    const { data: encrypted, sessionKey: sessionKeys } = await encryptMessage({
+        message: createMessage('Hello world!'),
+        publicKeys: [decryptedPrivateKey.toPublic()],
+        privateKeys: [decryptedPrivateKey],
+        sessionKey
+    });
+    const { data: decrypted, verified } = await decryptMessage({
+        message: await getMessage(encrypted),
+        publicKeys: [decryptedPrivateKey.toPublic()],
+        sessionKeys
+    });
+    t.is(decrypted, 'Hello world!');
+    t.is(verified, VERIFICATION_STATUS.SIGNED_AND_VALID);
+});
+
+test('it can encrypt and decrypt a message with session key without setting returnSessionKey with a detached signature', async (t) => {
+    const decryptedPrivateKey = await decryptPrivateKey(testPrivateKeyLegacy, '123');
+    const sessionKey = {
+        data: new Uint8Array([
+            197,
+            98,
+            157,
+            132,
+            15,
+            214,
+            78,
+            245,
+            90,
+            234,
+            71,
+            79,
+            135,
+            220,
+            222,
+            239,
+            118,
+            187,
+            199,
+            152,
+            163,
+            64,
+            239,
+            103,
+            4,
+            83,
+            21,
+            235,
+            121,
+            36,
+            163,
+            111
+        ]),
+        algorithm: 'aes256'
+    };
+    const { data: encrypted, sessionKey: sessionKeys, encryptedSignature } = await encryptMessage({
+        message: createMessage('Hello world!'),
+        publicKeys: [decryptedPrivateKey.toPublic()],
+        privateKeys: [decryptedPrivateKey],
+        detached: true,
+        sessionKey
     });
     const { data: decrypted, verified } = await decryptMessage({
         message: await getMessage(encrypted),
