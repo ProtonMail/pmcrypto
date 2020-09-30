febe23a15cd7d8a41dbbd1adcf3b395f0ca6e981
Marco <marco.martinoli@protonmail.com>
2020-09-30 12:18:12 +0000

    Use the message session key for encrypting detached signatures (#99)
    Also, add tests for the `sessionKey` and `returnSessionKey` options.

--- test/message/encryptMessage.spec.js
+++ test/message/encryptMessage.spec.js
@@ -1,5 +1,6 @@
 import test from 'ava';
 import '../helper';
+import { util } from 'openpgp';
 
 import { createMessage, getMessage, getSignature, verifyMessage } from '../../lib/message/utils';
 import encryptMessage from '../../lib/message/encrypt';
@@ -19,6 +20,23 @@ test('it can encrypt and decrypt a message', async (t) => {
         message: await getMessage(encrypted),
         publicKeys: [decryptedPrivateKey.toPublic()],
         privateKeys: [decryptedPrivateKey]
+    });
+    t.is(decrypted, 'Hello world!');
+    t.is(verified, VERIFICATION_STATUS.SIGNED_AND_VALID);
+});
+
+test('it can encrypt and decrypt a message with session keys', async (t) => {
+    const decryptedPrivateKey = await decryptPrivateKey(testPrivateKeyLegacy, '123');
+    const { data: encrypted, sessionKey: sessionKeys } = await encryptMessage({
+        message: createMessage('Hello world!'),
+        publicKeys: [decryptedPrivateKey.toPublic()],
+        privateKeys: [decryptedPrivateKey],
+        returnSessionKey: true
+    });
+    const { data: decrypted, verified } = await decryptMessage({
+        message: await getMessage(encrypted),
+        publicKeys: [decryptedPrivateKey.toPublic()],
+        sessionKeys
     });
     t.is(decrypted, 'Hello world!');
     t.is(verified, VERIFICATION_STATUS.SIGNED_AND_VALID);
@@ -61,6 +79,70 @@ test('it can encrypt and decrypt a message with an encrypted detached signature'
         encryptedSignature: await getMessage(encryptedSignature),
         publicKeys: [decryptedPrivateKey.toPublic()],
         privateKeys: [decryptedPrivateKey]
+    });
+    t.is(decrypted, 'Hello world!');
+    t.is(verified, VERIFICATION_STATUS.SIGNED_AND_VALID);
+});
+
+test('it can encrypt a message and decrypt it unarmored using session keys along with an encrypted detached signature', async (t) => {
+    const decryptedPrivateKey = await decryptPrivateKey(testPrivateKeyLegacy, '123');
+    const { message: encrypted, sessionKey: sessionKeys, encryptedSignature } = await encryptMessage({
+        message: createMessage('Hello world!'),
+        publicKeys: [decryptedPrivateKey.toPublic()],
+        privateKeys: [decryptedPrivateKey],
+        returnSessionKey: true,
+        detached: true,
+        armor: false
+    });
+    const { data: decrypted, verified } = await decryptMessage({
+        message: await getMessage(encrypted),
+        publicKeys: [decryptedPrivateKey.toPublic()],
+        encryptedSignature: await getMessage(encryptedSignature),
+        sessionKeys
+    });
+    t.is(decrypted, 'Hello world!');
+    t.is(verified, VERIFICATION_STATUS.SIGNED_AND_VALID);
+});
+
+test('it can encrypt and decrypt a message with session key without setting returnSessionKey', async (t) => {
+    const decryptedPrivateKey = await decryptPrivateKey(testPrivateKeyLegacy, '123');
+    const sessionKey = {
+        data: util.hex_to_Uint8Array('c5629d840fd64ef55aea474f87dcdeef76bbc798a340ef67045315eb7924a36f'),
+        algorithm: 'aes256'
+    };
+    const { data: encrypted } = await encryptMessage({
+        message: createMessage('Hello world!'),
+        publicKeys: [decryptedPrivateKey.toPublic()],
+        privateKeys: [decryptedPrivateKey],
+        sessionKey
+    });
+    const { data: decrypted, verified } = await decryptMessage({
+        message: await getMessage(encrypted),
+        publicKeys: [decryptedPrivateKey.toPublic()],
+        sessionKeys: sessionKey
+    });
+    t.is(decrypted, 'Hello world!');
+    t.is(verified, VERIFICATION_STATUS.SIGNED_AND_VALID);
+});
+
+test('it can encrypt and decrypt a message with session key without setting returnSessionKey with a detached signature', async (t) => {
+    const decryptedPrivateKey = await decryptPrivateKey(testPrivateKeyLegacy, '123');
+    const sessionKey = {
+        data: util.hex_to_Uint8Array('c5629d840fd64ef55aea474f87dcdeef76bbc798a340ef67045315eb7924a36f'),
+        algorithm: 'aes256'
+    };
+    const { data: encrypted, encryptedSignature } = await encryptMessage({
+        message: createMessage('Hello world!'),
+        publicKeys: [decryptedPrivateKey.toPublic()],
+        privateKeys: [decryptedPrivateKey],
+        detached: true,
+        sessionKey
+    });
+    const { data: decrypted, verified } = await decryptMessage({
+        message: await getMessage(encrypted),
+        publicKeys: [decryptedPrivateKey.toPublic()],
+        encryptedSignature: await getMessage(encryptedSignature),
+        sessionKeys: sessionKey
     });
     t.is(decrypted, 'Hello world!');
     t.is(verified, VERIFICATION_STATUS.SIGNED_AND_VALID);
