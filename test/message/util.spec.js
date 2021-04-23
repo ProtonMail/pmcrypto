ec076f12391d1225cc1694c9634b637591595961
larabr <7375870+larabr@users.noreply.github.com>
2021-04-23 14:50:00 +0200

    Return timestamp of first valid signature instead of last one

--- test/message/util.spec.js
+++ test/message/util.spec.js
@@ -8,45 +8,81 @@ import { createMessage } from '../../lib';
 
 const detachedSignatureFromTwoKeys = `-----BEGIN PGP SIGNATURE-----
 
-wnUEARYKAAYFAmCCm2wAIQkQ2WdF8CsF19UWIQQ1dEimEZorFsTlaU3ZZ0Xw
-KwXX1aQtAQDItpxggXie0/j68KSun/N+4v27j85NU541xATv0TV/SQEAqvH8
-WKpeRk/iCFEaAcTPvxxeVrLLWqhrCiLWKFFCsAXCdQQBFgoABgUCYIKbbAAh
-CRArVw5Kj4m/khYhBA7Q0tE0M2e8lpmxVytXDkqPib+SpC0A/0ISxzab1VbK
-XqYY4hV6v78cc0/mMQHx6S8Ywdn5v79lAQDmc77Yo+lHN7o0X155r8KtUwIi
-hOOX3oYkoNh2f/G0Cg==
-=uoHK
+wnUEARYKAAYFAmCCo8gAIQkQyQtnL+EYbekWIQTopSabUSqDUEv/FMHJC2cv
+4Rht6VeGAP4mUJl+WYN9nLE57YByTh95OmcZmwfgz5Z4R570YqTVngD/VBym
+icc7YREcxij1gC6SSAe8kgKW6oVOWzxJ8HkOSQrCdQQBFgoABgUCYIK9vQAh
+CRCGHCX3YYW5NRYhBErDPc6OYkUaNQCLhoYcJfdhhbk1W1QBAPhrkAjimO22
+jh1V2A8pRCOs53Ig/AMAFbN37BaAIEVKAP0SVMTL6zTxYJcxWNPog7Bv5lM4
+Px4G+hZ2Kia//qlgBg==
+=0aeU
 -----END PGP SIGNATURE-----`;
 
 const armoredPublicKey = `-----BEGIN PGP PUBLIC KEY BLOCK-----
 
-xjMEYIGi4hYJKwYBBAHaRw8BAQdAyVB7z3DBQoPkR9R65EJrWOM5dZ4W3LaC
-qILzlmjG7j7NEHRlc3QgPHRlc3RAYS5pdD7CjAQQFgoAHQUCYIGi4gQLCQcI
-AxUICgQWAAIBAhkBAhsDAh4BACEJENlnRfArBdfVFiEENXRIphGaKxbE5WlN
-2WdF8CsF19UExAD/Tpei9iGj+bBFjU9y4AXFZ+vxhYZp/S0pXgPOLcN7+WYA
-/ihD0b+EOkATniqREyFHzmKSIsy2oNlwSbekNUhJ23oCzjgEYIGi4hIKKwYB
-BAGXVQEFAQEHQDH6hToBmVyfGJT48RKhPt/SGSuzlzFUFtZzqKN9Pw9uAwEI
-B8J4BBgWCAAJBQJggaLiAhsMACEJENlnRfArBdfVFiEENXRIphGaKxbE5WlN
-2WdF8CsF19XbJwEAygvtdQPSZ9XOK/hdbhTGyO2KUwcFTKhsFAYiB2V45MUA
-/iWbqJFGXCN2KnGAFldAFHYz4Bpusz1GaHqIcoI8YSsD
-=C9dE
+xjMEYIKjXxYJKwYBBAHaRw8BAQdAbmODPSLO5tOI0GxfV+x5bgiiFriCcH3t
+6lbJkS+OzKbNEHRlc3QgPHRlc3RAYS5pdD7CjAQQFgoAHQUCYIKjXwQLCQcI
+AxUICgQWAAIBAhkBAhsDAh4BACEJEMkLZy/hGG3pFiEE6KUmm1Eqg1BL/xTB
+yQtnL+EYbenOlAEAn7A7RXQJ9FUzhuiOHeKqczdslgOO5LFcng1LuSIWn1UB
+ANWHrxnH63jnFLE82mfhpRZ5FYJ1fEXA9+3v6at3ZE8IzjgEYIKjXxIKKwYB
+BAGXVQEFAQEHQA5moGr1AKlYvKI+JpyB6W640eXpQFNSiV6LBjuMteNbAwEI
+B8J2BBgWCAAJBQJggqNfAhsMACEJEMkLZy/hGG3pFiEE6KUmm1Eqg1BL/xTB
+yQtnL+EYben97QD4hf6DttxyczHGqxGbboatBZ3IufJgFm6r2xNf9d9lSAD3
+U12oHbxyYUhapbFFkSIBo7DWJqWvx3iUEPqzY6jIAA==
+=ZWrn
 -----END PGP PUBLIC KEY BLOCK-----`;
 
-test('it verifies a message with multiple signatures and returns the valid signature timestamp', async (t) => {
-    const publicKey = (await openpgp.key.readArmored(armoredPublicKey)).keys[0];
+const armoredPublicKey2 = `-----BEGIN PGP PUBLIC KEY BLOCK-----
+
+xjMEYIKjgxYJKwYBBAHaRw8BAQdARyd9iDlrlozcTG144XFIjWozyWLz0KQv
+fL4lqIrwM8XNEHRlc3QgPHRlc3RAYi5pdD7CjAQQFgoAHQUCYIKjgwQLCQcI
+AxUICgQWAAIBAhkBAhsDAh4BACEJEIYcJfdhhbk1FiEESsM9zo5iRRo1AIuG
+hhwl92GFuTVPRAD6A6//tK5pLPa1d7mgsoqyJ9BZyTAmnzxtbIgmOU9/TDcB
+AI4cGBfCOLzRPw6L0il5Rt78TX1jz4Dlzu6YixJcJ2AFzjgEYIKjgxIKKwYB
+BAGXVQEFAQEHQMjb0Q1FWvHzj0hyOiEN5ndChBDceUqxmQ0wOYDVqq8JAwEI
+B8J4BBgWCAAJBQJggqODAhsMACEJEIYcJfdhhbk1FiEESsM9zo5iRRo1AIuG
+hhwl92GFuTXz4AEAqn4L+ayYgphejF/ZTRIseHPK+t521CT6NZKoVaHnTWQA
+/0+kMEB5d+CH3Mb54cUganYHPLj5utO2PexEJc3xARIG
+=IEm4
+-----END PGP PUBLIC KEY BLOCK-----`;
+
+test('it verifies a message with multiple signatures', async (t) => {
+    const publicKey1 = (await openpgp.key.readArmored(armoredPublicKey)).keys[0];
+    const publicKey2 = (await openpgp.key.readArmored(armoredPublicKey2)).keys[0];
     const { data, verified, signatureTimestamp, signatures, errors } = await verifyMessage({
         message: createMessage('hello world'),
         signature: await openpgp.signature.readArmored(detachedSignatureFromTwoKeys),
-        publicKeys: [publicKey] // the second public key is missing, expect only one signature to be verified
+        publicKeys: [publicKey1, publicKey2]
     });
     t.deepEqual(data, openpgp.util.str_to_Uint8Array('hello world'));
     t.is(verified, VERIFICATION_STATUS.SIGNED_AND_VALID);
     t.is(signatures.length, 2);
     t.is(errors, undefined);
-    const verifiedSignature = signatures
-        .map(({ packets: [sigPacket] }) => sigPacket)
-        .find((sigPacket) => sigPacket.issuerKeyId.equals(publicKey.getKeyId()));
-    t.is(verifiedSignature.verified, true);
-    t.is(signatureTimestamp, verifiedSignature.created);
+    const signaturePackets = signatures.map(({ packets: [sigPacket] }) => sigPacket);
+    signaturePackets.forEach(({ verified }) => {
+        t.is(verified, true);
+    });
+    t.is(signatureTimestamp, signaturePackets[0].created);
+});
+
+test('it verifies a message with multiple signatures and returns the timestamp of the valid signature', async (t) => {
+    const publicKey1 = (await openpgp.key.readArmored(armoredPublicKey)).keys[0];
+    const publicKey2 = (await openpgp.key.readArmored(armoredPublicKey2)).keys[0];
+    const { data, verified, signatureTimestamp, signatures, errors } = await verifyMessage({
+        message: createMessage('hello world'),
+        signature: await openpgp.signature.readArmored(detachedSignatureFromTwoKeys),
+        publicKeys: [publicKey1] // the second public key is missing, expect only one signature to be verified
+    });
+    t.deepEqual(data, openpgp.util.str_to_Uint8Array('hello world'));
+    t.is(verified, VERIFICATION_STATUS.SIGNED_AND_VALID);
+    t.is(signatures.length, 2);
+    t.is(errors, undefined);
+    const signaturePackets = signatures.map(({ packets: [sigPacket] }) => sigPacket);
+    const validSignature = signaturePackets.find((sigPacket) => sigPacket.issuerKeyId.equals(publicKey1.getKeyId()));
+    const invalidSignature = signaturePackets.find((sigPacket) => sigPacket.issuerKeyId.equals(publicKey2.getKeyId()));
+    t.is(validSignature.verified, true);
+    t.is(signatureTimestamp, validSignature.created);
+    t.is(invalidSignature.verified, null);
+    t.not(signatureTimestamp, invalidSignature.created);
 });
 
 test('it does not verify a message given wrong public key', async (t) => {
