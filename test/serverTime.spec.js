0f446cd0ff4ea97b21cbb930f3f96edacd9f5b5b
larabr <7375870+larabr@users.noreply.github.com>
2021-04-27 13:10:00 +0200

    Refuse server time updates too far from local time

---
+++ test/serverTime.spec.js
@@ -0,0 +1,29 @@
+import test from 'ava';
+
+import { updateServerTime, serverTime } from '../lib';
+
+const MILLISECONDS_1_HOUR = 3600 * 1000;
+const oneHourAgo = new Date(Date.now() - MILLISECONDS_1_HOUR);
+const oneDayAgo = new Date(Date.now() - MILLISECONDS_1_HOUR * 25);
+const oneDayAhead = new Date(Date.now() + MILLISECONDS_1_HOUR * 25);
+
+test('it correctly updates the server time', async (t) => {
+    const currentServerTime = serverTime();
+    const updatedServerTime = updateServerTime(oneHourAgo);
+    t.not(currentServerTime, updatedServerTime);
+    t.is(updatedServerTime, oneHourAgo);
+    t.is(updatedServerTime, serverTime());
+});
+
+test('it does not allow to set an older server time', async (t) => {
+    const currentTime = new Date();
+    const updatedServerTime = updateServerTime(currentTime);
+    t.is(currentTime, updatedServerTime);
+    t.is(updateServerTime(oneHourAgo), currentTime);
+});
+
+test('it does not allow to set a server time too far from the local time', async (t) => {
+    const currentServerTime = serverTime();
+    t.is(updateServerTime(oneDayAgo), currentServerTime);
+    t.is(updateServerTime(oneDayAhead), currentServerTime);
+});
