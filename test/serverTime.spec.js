50606a8bf58f2896a7f76be28580dafd3459b5bf
larabr <7375870+larabr@users.noreply.github.com>
2021-04-27 14:14:19 +0200

    Throw error if time difference is too large

--- test/serverTime.spec.js
+++ test/serverTime.spec.js
@@ -7,7 +7,7 @@ const oneHourAgo = new Date(Date.now() - MILLISECONDS_1_HOUR);
 const oneDayAgo = new Date(Date.now() - MILLISECONDS_1_HOUR * 25);
 const oneDayAhead = new Date(Date.now() + MILLISECONDS_1_HOUR * 25);
 
-test('it correctly updates the server time', async (t) => {
+test('it correctly updates the server time', (t) => {
     const currentServerTime = serverTime();
     const updatedServerTime = updateServerTime(oneHourAgo);
     t.not(currentServerTime, updatedServerTime);
@@ -15,15 +15,14 @@ test('it correctly updates the server time', async (t) => {
     t.is(updatedServerTime, serverTime());
 });
 
-test('it does not allow to set an older server time', async (t) => {
+test('it does not allow to set an older server time', (t) => {
     const currentTime = new Date();
     const updatedServerTime = updateServerTime(currentTime);
     t.is(currentTime, updatedServerTime);
     t.is(updateServerTime(oneHourAgo), currentTime);
 });
 
-test('it does not allow to set a server time too far from the local time', async (t) => {
-    const currentServerTime = serverTime();
-    t.is(updateServerTime(oneDayAgo), currentServerTime);
-    t.is(updateServerTime(oneDayAhead), currentServerTime);
+test('it does not allow to set a server time too far from the local time', (t) => {
+    t.throws(() => updateServerTime(oneDayAgo));
+    t.throws(() => updateServerTime(oneDayAhead));
 });
