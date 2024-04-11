3d097af7e966b0f3c5bb85850ee11948b7b4a959
larabr <7375870+larabr@users.noreply.github.com>
2024-04-11 16:15:40 +0200

    `argon2`: reload wasm module after timeout when using memory heavy params
    We manually reload the module if no memory-heavy (128MB+) argon2 computation has been requested
    in a while, to deallocate the memory.
    This is better than reloading the module every time (automatically done if the memory exceeds
    `Argon2S2K.ARGON2_WASM_MEMORY_THRESHOLD_RELOAD`),
    as doing so comes with a performance hit.

--- lib/crypto/argon2.ts
+++ lib/crypto/argon2.ts
@@ -12,8 +12,36 @@ export interface Argon2Options {
     params: Argon2Params
 }
 
+// We manually reload the module if no memory-heavy (128MB+) argon2 computation has been requested in a while,
+// to deallocate the memory.
+// This is better than reloading the module every time (automatically done if the memory exceeds `Argon2S2K.ARGON2_WASM_MEMORY_THRESHOLD_RELOAD`),
+// as doing so comes with a performance hit.
+const SECOND = 1000;
+const TimeoutHandler = {
+    id: undefined,
+    cancelReloadingTimeout: (memoryExponent: number) => (
+        memoryExponent > ARGON2_PARAMS.MINIMUM.memoryExponent && clearTimeout(TimeoutHandler.id)
+    ),
+    setupReloadingTimeout: (memoryExponent: number) => {
+        const shouldReloadAfterTimeout = memoryExponent > ARGON2_PARAMS.MINIMUM.memoryExponent &&
+            memoryExponent < Argon2S2K.ARGON2_WASM_MEMORY_THRESHOLD_RELOAD;
+        // @ts-ignore NodeJS.Timeout typedef interfering
+        TimeoutHandler.id = shouldReloadAfterTimeout ?
+            setTimeout(
+                () => Argon2S2K.reloadWasmModule(),
+                10 * SECOND
+            ) as any as number
+            : undefined;
+    }
+};
+
 export async function argon2({ password, salt, params = ARGON2_PARAMS.RECOMMENDED }: Argon2Options) {
+    TimeoutHandler.cancelReloadingTimeout(params.memoryExponent);
+
     const s2k = new Argon2S2K({ ...defaultConfig, s2kArgon2Params: params });
     s2k.salt = salt;
-    return s2k.produceKey(password, params.tagLength);
+    const result = await s2k.produceKey(password, params.tagLength);
+
+    TimeoutHandler.setupReloadingTimeout(params.memoryExponent);
+    return result;
 }
